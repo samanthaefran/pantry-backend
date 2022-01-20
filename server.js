8e63d1a574c9f2226190d8fbf6a7318bc6da8081
samantha frangi <sefrangi13@gmail.com>
2022-01-20 17:38:19 -0500

    wip: successfully adds index route

--- server.js
+++ server.js
@@ -15,11 +15,31 @@ mongoose.connect(DATABASE_URL)
 
 mongoose.connection
   .on("open", () => console.log("open"))
-  .on("close", () => console.log ("close"))
+  .on("close", () => console.log("close"))
   .on("error", (error) => console.log(error))
+
+const PantrySchema = new mongoose.Schema({
+  name: String,
+  quantity: String,
+})
+
+const Pantry = mongoose.model("Pantry", PantrySchema)
+
+app.use(cors())
+app.use(morgan("dev"))
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
   res.send('im alive!')
+})
+
+app.get("/pantry", async (req, res) => {
+  try {
+    res.json(await Pantry.find({}))
+  } catch (error) {
+    res.status(400).json(error)
+  }
 })
 
 app.listen(PORT, () => console.log(`${PORT}`))

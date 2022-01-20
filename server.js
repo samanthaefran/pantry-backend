49b04ddde5cc4c370fc3b215c9f88a1a9a5d81f1
samantha frangi <sefrangi13@gmail.com>
2022-01-20 13:27:51 -0500

    wip: establishes connection to db

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 require('dotenv').config()
 
-const { PORT = 3001 } = process.env
+const { PORT = 3001, DATABASE_URL } = process.env
 
 const express = require('express')
 
 const app = express()
+
+const mongoose = require('mongoose')
+
+const cors = require('cors')
+const morgan = require('morgan')
+
+mongoose.connect(DATABASE_URL)
+
+mongoose.connection
+  .on("open", () => console.log("open"))
+  .on("close", () => console.log ("close"))
+  .on("error", (error) => console.log(error))
 
 app.get('/', (req, res) => {
   res.send('im alive!')
